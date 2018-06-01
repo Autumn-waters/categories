ebc5c4dcd33760823ef9b40252cee331c4a6819a
qiuxinshu <1044812735@qq.com>
2018-06-01 15:14:24 +0800

    xixi

--- src/Category.php
+++ src/Category.php
@@ -58,13 +58,13 @@ class Category
      * */
     public function find_lower($id,$level=null)
     {
-        $p_data = $this->model->find($id);
+        $member = $this->model->find($id);
         $where = [
-            ['le','>',$p_data->le],
-            ['ri','<',$p_data->ri]
+            ['le','>',$member->le],
+            ['ri','<',$member->ri]
         ];
         if($level){
-            $where[] = ['level','<=',$p_data->level+$level];
+            $where[] = ['level','<=',$member->level+$level];
         }
 
         return $this->model->where($where)->select();
@@ -76,13 +76,13 @@ class Category
      * */
     public function find_uper($id,$level=null)
     {
-        $p_data = $this->model->find($id);
+        $member = $this->model->find($id);
         $where = [
-            ['le','<',$p_data->le],
-            ['ri','>',$p_data->ri],
+            ['le','<',$member->le],
+            ['ri','>',$member->ri],
         ];
         if($level){
-            $where[] = ['level','>=',$p_data->level-$level];
+            $where[] = ['level','>=',$member->level-$level];
         }
 
         return $this->model->where($where)->select();
@@ -96,43 +96,85 @@ class Category
      * */
     public function update_user($userid,$pid)
     {
-        $user = $this->model->find($userid);
-        $p_data = $this->model->find($pid);
+        $member = $this->model->find($userid);
+        $parent = $this->model->find($pid);
 
-        $chirdArr = $this->find_chird_collection($user->le,$user->ri);
+        $chirdArr = $this->find_chird_collection($member->le,$member->ri);
 
-        $length = $user->ri-$user->le+1;
+        $length = $member->ri-$member->le+1;
 
-        $where = [
-            ['ri','>',$user->ri],
-            ['ri','<',$p_data->ri],
-        ];
+        if($parent->ri-$member->ri>0){
+            $this->update_right_user($member,$parent,$length,$chirdArr);
+        }else{
+            $this->update_left_user($member,$parent,$length,$chirdArr);
+        }
 
-        $where1 = [
-            ['le','>',$user->ri],
-            ['le','<',$p_data->ri],
-        ];
-        $this->model->where($where1)->setDec('le',$length);
-        $this->model->where($where)->setDec('ri',$length);
+        $level = $parent->level+1;
 
-        $new_length = $p_data->ri-$user->le-$length;
-
-        $this->model->whereIn('id',$chirdArr)->setInc('le',$new_length);
-        $this->model->whereIn('id',$chirdArr)->setInc('ri',$new_length);
-
-        $level = $p_data->level+1;
-
-        $new_level = $user->level-$level;
+        $new_level = $member->level-$level;
 
         $levelwhere = [
-            ['le','>=',$user->le],
-            ['ri','<=',$user->ri],
+            ['le','>=',$member->le],
+            ['ri','<=',$member->ri],
         ];
         $this->model->where($levelwhere)->setDec('level',$new_level);
 
         $this->model->where('id',$userid)->update(['pid'=>$pid,'level'=>$level]);
+    }
+    /*
+     * 更改到右边用户
+     * @param $userid要更改的用户
+     * @param $pid更改在哪个用户下
+     * */
+    public function update_right_user($member,$parent,$length,$chirdArr)
+    {
+        $where = [
+            ['ri','>',$member->ri],
+            ['ri','<',$parent->ri],
+        ];
+
+        $where1 = [
+            ['le','>',$member->ri],
+            ['le','<',$parent->ri],
+        ];
+        $this->model->where($where1)->setDec('le',$length);
+        $this->model->where($where)->setDec('ri',$length);
+
+        $new_length = $parent->ri-$member->le-$length;
+
+        $this->model->whereIn('id',$chirdArr)->setInc('le',$new_length);
+        $this->model->whereIn('id',$chirdArr)->setInc('ri',$new_length);
+
+
         //return true;
     }
+
+    /*
+     * 更改到左边的用户
+     * @param $userid要更改的用户
+     * @param $pid更改在哪个用户下
+     * */
+    public function update_left_user($member,$parent,$length,$chirdArr)
+    {
+
+        $where = [
+            ['ri','<',$member->ri],
+            ['ri','>=',$parent->ri],
+        ];
+
+        $where1 = [
+            ['le','<',$member->le],
+            ['le','>',$parent->ri],
+        ];
+        $this->model->where($where1)->setInc('le',$length);
+        $this->model->where($where)->setInc('ri',$length);
+
+        $new_length = $parent->ri-$member->le;
+
+        $this->model->whereIn('id',$chirdArr)->setInc('le',$new_length);
+        $this->model->whereIn('id',$chirdArr)->setInc('ri',$new_length);
+    }
+
     /*
      * 查询下级集合
      * @param $userid用户id
